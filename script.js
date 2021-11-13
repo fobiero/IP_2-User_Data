ce9b706520a8a6cfbafcd98e5d9b02b4a17908b9
feLycks <felix.obiero@student.moringaschool.com>
2021-11-14 01:15:47 +0300

    start script

---
+++ script.js
@@ -0,0 +1,6 @@
+function checkData() {
+    const day = document.getElementById('day').value;
+    const month = document.getElementById('month').value;
+    const year = document.getElementById('year').value;
+
+}
